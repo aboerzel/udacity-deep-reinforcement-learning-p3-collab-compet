1d85df8e3574ee587075705e21e66bcb70acb24a
Andreas <andreas.boerzel@gmx.de>
2021-02-24 20:27:48 +0100

    feat: Tennis.ipynb

--- model.py
+++ model.py
@@ -24,6 +24,7 @@ class Actor(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
+        self.state_size = state_size
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
@@ -38,6 +39,9 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
+        if len(state) == self.state_size:
+            state = state.unsqueeze(0)
+
         x = F.leaky_relu(self.fc1(state))
         x = self.bn1(x)
         x = F.leaky_relu(self.fc2(x))
