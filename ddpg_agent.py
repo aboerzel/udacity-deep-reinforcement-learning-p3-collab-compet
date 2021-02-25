186b3769ce0e42706c37ed446967d48d5bf3d885
Andreas <andreas.boerzel@gmx.de>
2021-02-25 20:22:35 +0100

    feat: Tennis.ipynb

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -44,10 +44,10 @@ class Agent:
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
-    def step(self, states, actions, rewards, next_states, dones):
+    def step(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
-        self.memory.add(states, actions, rewards, next_states, dones)
+        self.memory.add(state, action, reward, next_state, done)
 
         # Learn, if enough samples are available in memory
         if len(self.memory) > BATCH_SIZE:
